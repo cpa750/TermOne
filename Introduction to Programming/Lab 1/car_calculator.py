e44799201a39fcc0a63285a25b6a3b2f46bd426f
Cian <ciantheskier@gmail.com>
2018-10-05 13:23:11 +0100

    Reformatted code so it's not shitty.

--- Introduction to Programming/Lab 1/car_calculator.py
+++ Introduction to Programming/Lab 1/car_calculator.py
@@ -1,14 +1,15 @@
 DEALER_PREP = 1250
 DESTINATION_CHARGE = 500
 CLEANING_CHARGE = 750
+TAX_RATE = 0.15
+LICENCE_RATE = 0.075
 
 base_car_amount = float(input("Enter the base amount of the car: $"))
 
-car_tax = base_car_amount*.15
-licence = base_car_amount*.075
+car_tax = base_car_amount*TAX_RATE
+licence = base_car_amount*LICENCE_RATE
 
 final_amount = DEALER_PREP + DESTINATION_CHARGE + CLEANING_CHARGE + car_tax + licence + base_car_amount
-final_amount = "$" + str(round(final_amount, 2))
 
 
-print("The final amount is", final_amount, sep=" ")
+print("The final amount is $", str(final_amount), sep="")
--- Introduction to Programming/Lab 1/tipper.py
+++ Introduction to Programming/Lab 1/tipper.py
@@ -1,9 +1,26 @@
 from math import ceil
 
+SMALL_TIP_RATE = 0.15
+LARGE_TIP_RATE = 0.2
+
+def apply_tip(total):
+    """
+    Function that applies two tip rates to a total, then rounds that number.
+    Returns the two new totals as a tuple.
+    """
+    small_tip = round(total*SMALL_TIP_RATE, 2)
+    large_tip = round(total*LARGE_TIP_RATE, 2)
+
+    return (small_tip, large_tip)
+
 total = float(input("Enter the total bill amount: "))
 
-print("15% tip:", total*0.15, sep=" ")
-print("20% tip:", total*0.2, sep=" ")
+tipped_total = apply_tip(total)
+print("15% tipped total: ${}".format(tipped_total[0]))
+print("20% tipped total: ${}".format(tipped_total[1]))
 
-print("15% tip, bill rouded up:", round(ceil(total)*0.15, 2), sep=" ")
-print("20% tip, bill rouded up:", round(ceil(total)*0.2, 2), sep=" ")
+total = ceil(total)
+
+tipped_total = apply_tip(total)
+print("15% tipped total with initial amount rounded up: ${}".format(tipped_total[0]))
+print("20% tipped total with initial amount rounded up: ${}".format(tipped_total[1]))
